0ad60fc8097c3317f8a37a95be06987e56eb0ffd
ABHIMANYU ROY <54316830+abhimanyu1214@users.noreply.github.com>
2021-04-03 09:55:10 +0530

    Create config.go

---
+++ config.go
@@ -0,0 +1,14 @@
+package config
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+func GetMySQLDB() (db *sql.DB, err error) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := "123456"
+	dbName := "learngodb"
+	db, err = sql.Open(dbDriver, dbUser + ":" + dbPass + "@/" + dbName)
+	return
+}
